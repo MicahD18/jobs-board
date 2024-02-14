1923cd1e3831066758a74d8a054ae06bf2dea5d2
MicahD18 <70763379+MicahD18@users.noreply.github.com>
2024-02-14 12:43:55 -0700

    Added header with styles and toggle switch for light/dark mode

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div
+      style={{ backgroundColor: "var(--bg-color)", color: "var(--text-color)" }}
+    >
+      <p>Home works!</p>
+    </div>
+  );
+};
+
+export default Home;
